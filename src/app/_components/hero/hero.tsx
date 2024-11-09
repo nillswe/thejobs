5bdcdab13d412351af67c18ef1339c615db44408
Nill <niltonrck@gmail.com>
2024-11-08 23:16:46 -0300

    feat: add base structure of the home page

--- src/app/_components/hero/hero.tsx
+++ src/app/_components/hero/hero.tsx
@@ -1,16 +1,31 @@
+import { DM_Sans } from 'next/font/google'
+
+import { merge } from '@/utils'
+
+const dmSans = DM_Sans({ subsets: ['latin'], weight: ['700'] })
+
 export const Hero = () => {
   return (
-    <div className='hero flex-1'>
-      <div className='hero-content text-center'>
-        <div className='max-w-md'>
-          <h1 className='text-5xl font-bold'>Home page</h1>
-          <p className='py-6'>
+    <section className='h-[31rem] bg-base-300 flex w-full justify-center items-center'>
+      <div className='text-center flex flex-col items-center justify-center'>
+        <header className='max-w-lg'>
+          <h1 className={merge(['text-[7.6rem] font-bold tracking-[-0.8rem]', dmSans.className])}>
+            thejobs
+          </h1>
+          <p className='mb-16'>
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
             exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
           </p>
-          <button className='btn btn-primary'>Get Started</button>
-        </div>
+        </header>
+
+        <footer className='join max-w-full w-[40rem]'>
+          <input
+            placeholder='React frontend developer'
+            className='input input-lg input-bordered join-item flex-1'
+          />
+          <button className='btn btn-lg join-item btn-secondary'>Find your dream job</button>
+        </footer>
       </div>
-    </div>
+    </section>
   )
 }
